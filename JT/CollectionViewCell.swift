7ea53cc3f824a0d8a57c28f53a54c7a538642a88
kuanhuachen <kuanhuachen@doits-MacBook-Pro.local>
2017-10-02 18:11:47 +0800

    test push

---
+++ JT/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  JT
+//
+//  Created by kuanhuachen on 2017/10/2.
+//  Copyright © 2017年 D.O. Information Technology Corp. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CollectionViewCell: JTAppleCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+}
---
+++ JT/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  JT
+//
+//  Created by kuanhuachen on 2017/10/2.
+//  Copyright © 2017年 D.O. Information Technology Corp. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class ViewController: UIViewController {
+    
+    let formatter = DateFormatter()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        cell.dateLabel.text = cellState.text
+        
+        return cell
+    }
+    
+}
+
